41c139fe5af9265113f4346a3ed62f0c8391aef1
Colin Lin <colincool100@gmail.com>
2022-01-03 11:34:31 -0500

    fixed undefined issue currentTab.url

--- src/chromeServices/TimeTracker.ts
+++ src/chromeServices/TimeTracker.ts
@@ -10,25 +10,28 @@ if (typeof window === 'undefined')
     setInterval(async function () {
         let currentTab = await getCurrentTab();
         
-        if (currentTab !== undefined && currentTab.url !== undefined && currentTab.url !== null)
+        if (currentTab!==null && currentTab?.url!==null)
         {
-            // @ts-ignore: Object is possibly 'null'.
-            let domain = currentTab.url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+            
+            let matches = currentTab.url?.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/);
+            if(matches){
+                let domain = matches[1]
     
-            chrome.storage.sync.get([domain], function(result) {
-                if (result === undefined || JSON.stringify(result) === "{}")
-                {
-                    chrome.storage.sync.set({[domain]: 10000}, function() {
-                        console.log(domain + ' is set to ' + 10000);
-                    });
-                }
-                else
-                {
-                    chrome.storage.sync.set({[domain]: result[domain] + 10000}, function() {
-                        console.log(domain + ' is set to ' + (result[domain] + 10000));
-                    });
-                }
-            });
+                chrome.storage.sync.get([domain], function(result) {
+                    if (result === undefined || JSON.stringify(result) === "{}")
+                    {
+                        chrome.storage.sync.set({[domain]: 10000}, function() {
+                            console.log(domain + ' is set to ' + 10000);
+                        });
+                    }
+                    else
+                    {
+                        chrome.storage.sync.set({[domain]: result[domain] + 10000}, function() {
+                            console.log(domain + ' is set to ' + (result[domain] + 10000));
+                        });
+                    }
+                });
+            }
         }
     }, 10000);
 }
