42bc7f5f6749d33ac2b34738401669cb0465efbd
Henry Zhang <henryzhang757@gmail.com>
2022-01-03 10:52:34 -0500

    Added if statement so poller doesn't activate on newtab

--- src/chromeServices/TimeTracker.ts
+++ src/chromeServices/TimeTracker.ts
@@ -54,35 +54,37 @@ async function getCurrentTab() {
     return tab;
 }
 
-
-
-setInterval(async function () {
-    let currentTab = await getCurrentTab();
-    
-    if (currentTab !== undefined && currentTab.url !== undefined && currentTab.url !== null)
-    {
-        // @ts-ignore: Object is possibly 'null'.
-        let domain = currentTab.url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+// only runs on service worker window, doesn't work on new tab page (which is intended)
+if (typeof window === 'undefined')
+{
+    setInterval(async function () {
+        let currentTab = await getCurrentTab();
         
-        // Given url = "http://www.google.com/", domain == "www.google.com" 
-        //console.log(domain)
-
-        chrome.storage.sync.get([domain], function(result) {
-            if (result === undefined || JSON.stringify(result) === "{}")
-            {
-                chrome.storage.sync.set({[domain]: 10000}, function() {
-                    console.log(domain + ' is set to ' + 10000);
-                });
-            }
-            else
-            {
-                chrome.storage.sync.set({[domain]: result[domain] + 10000}, function() {
-                    console.log(domain + ' is set to ' + (result[domain] + 10000));
-                });
-            }
-        });
-    }
-}, 10000);
+        if (currentTab !== undefined && currentTab.url !== undefined && currentTab.url !== null)
+        {
+            // @ts-ignore: Object is possibly 'null'.
+            let domain = currentTab.url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+            
+            // Given url = "http://www.google.com/", domain == "www.google.com" 
+            //console.log(domain)
+    
+            chrome.storage.sync.get([domain], function(result) {
+                if (result === undefined || JSON.stringify(result) === "{}")
+                {
+                    chrome.storage.sync.set({[domain]: 10000}, function() {
+                        console.log(domain + ' is set to ' + 10000);
+                    });
+                }
+                else
+                {
+                    chrome.storage.sync.set({[domain]: result[domain] + 10000}, function() {
+                        console.log(domain + ' is set to ' + (result[domain] + 10000));
+                    });
+                }
+            });
+        }
+    }, 10000);
+}
  
 
 export {}
