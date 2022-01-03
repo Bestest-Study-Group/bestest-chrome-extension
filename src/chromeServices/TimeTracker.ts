89fa1fb2f5a1388e77f4661810721a3da5af3e0e
Henry Zhang <henryzhang757@gmail.com>
2022-01-02 21:07:28 -0500

    added timetracking as injected content script

--- src/chromeServices/TimeTracker.ts
+++ src/chromeServices/TimeTracker.ts
@@ -1,5 +1,29 @@
-console.log("TEST");
-
 // get the current url
+const host = window.location.host
+
+var totalTime: number = 0;
+
+console.log(host);
+
+chrome.storage.sync.get([host], function(result) {
+    if (result === undefined)
+    {
+        console.log("Value is currently undefined")
+        totalTime = 0;
+    }
+    else
+    {
+        totalTime = result[host];
+        console.log('Value currently is ' + totalTime);
+    }
+});
+
+setInterval(function () {
+    totalTime += 10000;
+    chrome.storage.sync.set({[host]: totalTime}, function() {
+        console.log(host + ' is set to ' + totalTime);
+    });
+}, 10000);
+
 
 export {}
