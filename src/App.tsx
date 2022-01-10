42a511fc6ea29b3ac9c20d191d702f1cb7a72b77
Mitravasu Prakash <43656604+Mitravasu@users.noreply.github.com>
2022-01-09 23:18:04 -0500

    [BCE-9] TopSites show in CardRow instead of script.

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,12 @@ import { useEffect, useState } from 'react'
 import Card from './templateComponents/Card'
 import CardRow from './templateComponents/CardRow';
 import TimeDisplay from './templateComponents/TimeDisplay';
-import './templateComponents/TopSites';
 
 function App() {
-	let cards = []
-	for(let i = 0; i < 10; i++){
-		cards.push(
-			<Card>
-				<h1 className='cardText'></h1>
-			</Card>
-		)
-	}
 	return (
 		<div className="bg-slate-400 h-screen">
 			<TimeDisplay/>
-			<CardRow>
-				{cards}
-			</CardRow>
-			<p id='TopSitesDisplay'></p>
+			<CardRow></CardRow>
 		</div>
 	);
 }
